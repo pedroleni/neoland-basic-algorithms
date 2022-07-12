3a52638504518b564fe8f31c7322a83d58723b42
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-12 20:37:59 +0200

    Iteration#2

---
+++ app.js
@@ -0,0 +1,58 @@
+/*------------------------------------------------------------------------
+ ----------------**Iteración #1: Variables--------------------------------
+ -----------------------------------------------------------------------*/
+
+let myFavoriteHero="Hulk"
+let x = 50;
+let h = 5, y=10;
+let z = h + y;
+
+/*-----------------------------------------------------------------------
+----------------**Iteración #2: varibles avanzadas**---------------------
+-----------------------------------------------------------------------*/
+
+//Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+const character = { name: 'Jack Sparrow', age: 10};
+console.log(character);
+character.age = 25;
+console.log(character);
+
+
+//Muestralos por consola de esta forma: 'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+//Muestralos por consola de esta forma:'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+console.log("soy " + firstName + " " + lastName + ",tengo " + age + " años y me gustan los lobos");
+
+console.log(`soy ${firstName} ${lastName}, tengo ${age} años y me gust
+an los lobos`);
+
+//Dado el siguiente javascript, imprime con un console.log la suma del precio de ambos juguetes.
+
+const toy1 = { name: 'Buss myYear', price: 19 };
+const toy2 = { name: 'Rallo mcKing', price: 29 };
+
+console.log("Total Price", toy1.price + toy2.price);
+
+/*Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
+y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
+basePrice más el valor de la variable globalBasePrice.*/
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+console.log(car1);
+console.log(car2);
+
+globalBasePrice = 25000;
+
+car1.finalPrice = car1.basePrice + globalBasePrice;
+console.log("MODIFICADO", car1);
+
+
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log("MODIFICADO", car2);
+
+
